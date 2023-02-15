28df978e22b0ba4368b855e21ac3d84e7980f729
thomais <tellisjohnson00@gmail.com>
2023-02-15 17:53:24 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int BigNumber = 50;
+            int val = 0;
+            while (val < 10)
+                {
+                foreach (int value in Enumerable.Range(1, BigNumber))
+                {
+
+                    if (value % 3 == 0 && value % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                        if (value == BigNumber)
+                        {
+                            val = 10;
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (value % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+
+                        if (value == BigNumber)
+                        {
+                            val = 10;
+                            Console.ReadLine();
+                        }
+                    }
+                    else if (value % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                        if (value == BigNumber)
+                        {
+                            val = 10;
+                            Console.ReadLine();
+                        }
+                    }
+                    else {
+                        Console.WriteLine(value);
+                        if (value == BigNumber)
+                        {
+                            val = 10;
+                            Console.ReadLine();
+                        }
+                    }
+                    
+                }
+            
+            }
+        }
+    }
+}
